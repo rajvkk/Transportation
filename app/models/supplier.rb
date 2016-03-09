e57291e291e82b5444fd5f70cf8a243c74ec2dfb
Varun <varunrajukk@gmail.com>
2016-03-09 11:53:49 -0600

    many-many relations

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,4 @@
 class Supplier < ActiveRecord::Base
+	belongs_to :store
+	belongs_to :warehouse
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,2 +1,4 @@
 class Warehouse < ActiveRecord::Base
+  has_many :suppliers
+  has_many :stores, through: :suppliers
 end
