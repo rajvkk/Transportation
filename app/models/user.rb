5fc07b501240487a2864ebf534afd979fb784294
Varun <varunrajukk@gmail.com>
2016-03-10 17:57:21 -0600

    set api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
+
+require 'digest/sha1'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable
+
+  before_create :set_api_key
+
+  def set_api_key(username)
+  	self.api_key = Digest::SHA1.hexdigest(username)
+  end
+
 end
